359fc4d0bf71b05fae3891dbeec48879bbc1a72a
Nilesh9492 <nilesh94041@gmail.com>
2021-10-09 09:03:46 +0530

    Add project files.

---
+++ Employee Attendnence/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Employee_Attendnence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPresent = 1;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == isPresent)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
